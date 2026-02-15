c0b6f3dcc8f499d808625b8c0b97a65b70d6e138
SpideryBook7 <cristian.huerta.dev@gmail.com>
2026-02-15 00:12:33 -0600

    Build: Upgrade Java to 17 and add professional colors

--- app/build/tmp/kapt3/stubs/debug/com/spiderybook/ui/home/HomeViewModel.java
+++ app/build/tmp/kapt3/stubs/debug/com/spiderybook/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ import com.spiderybook.util.Resource;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 import javax.inject.Inject;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0002\b\u0003\b\u0007\u0018\u00002\u00020\u0001B\u0017\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u000e\u0010\u000e\u001a\u00020\u000f2\u0006\u0010\u0010\u001a\u00020\tJ\u0006\u0010\u0011\u001a\u00020\u000fR\u0014\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\t0\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\n\u001a\b\u0012\u0004\u0012\u00020\t0\u000b\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\r\u00a8\u0006\u0012"}, d2 = {"Lcom/spiderybook/ui/home/HomeViewModel;", "Lcom/spiderybook/ui/common/BaseViewModel;", "homeRepository", "Lcom/spiderybook/data/repository/HomeRepository;", "pluginManager", "Lcom/spiderybook/plugins/PluginManager;", "(Lcom/spiderybook/data/repository/HomeRepository;Lcom/spiderybook/plugins/PluginManager;)V", "_selectedProvider", "Landroidx/lifecycle/MutableLiveData;", "", "selectedProvider", "Landroidx/lifecycle/LiveData;", "getSelectedProvider", "()Landroidx/lifecycle/LiveData;", "loadHomePage", "", "apiName", "loadProviders", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000B\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0010\u0002\n\u0002\b\u0003\b\u0007\u0018\u00002\u00020\u0001B\u0017\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u000e\u0010\u0017\u001a\u00020\u00182\u0006\u0010\u0019\u001a\u00020\nJ\u0006\u0010\u001a\u001a\u00020\u0018R\u001a\u0010\u0007\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001a\u0010\u000b\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\r0\f0\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u000e\u001a\b\u0012\u0004\u0012\u00020\n0\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u001d\u0010\u000f\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\n0\t0\u0010\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0012R\u001d\u0010\u0013\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\r0\f0\u0010\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0012R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\u0015\u001a\b\u0012\u0004\u0012\u00020\n0\u0010\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\u0012\u00a8\u0006\u001b"}, d2 = {"Lcom/spiderybook/ui/home/HomeViewModel;", "Lcom/spiderybook/ui/common/BaseViewModel;", "homeRepository", "Lcom/spiderybook/data/repository/HomeRepository;", "pluginManager", "Lcom/spiderybook/plugins/PluginManager;", "(Lcom/spiderybook/data/repository/HomeRepository;Lcom/spiderybook/plugins/PluginManager;)V", "_availableProviders", "Landroidx/lifecycle/MutableLiveData;", "", "", "_homePage", "Lcom/spiderybook/util/Resource;", "Lcom/spiderybook/domain/model/HomePageResponse;", "_selectedProvider", "availableProviders", "Landroidx/lifecycle/LiveData;", "getAvailableProviders", "()Landroidx/lifecycle/LiveData;", "homePage", "getHomePage", "selectedProvider", "getSelectedProvider", "loadHomePage", "", "apiName", "loadProviders", "app_debug"})
 @dagger.hilt.android.lifecycle.HiltViewModel()
 public final class HomeViewModel extends com.spiderybook.ui.common.BaseViewModel {
     @org.jetbrains.annotations.NotNull()
@@ -21,6 +21,14 @@ public final class HomeViewModel extends com.spiderybook.ui.common.BaseViewModel
     private final androidx.lifecycle.MutableLiveData<java.lang.String> _selectedProvider = null;
     @org.jetbrains.annotations.NotNull()
     private final androidx.lifecycle.LiveData<java.lang.String> selectedProvider = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<com.spiderybook.util.Resource<com.spiderybook.domain.model.HomePageResponse>> _homePage = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.LiveData<com.spiderybook.util.Resource<com.spiderybook.domain.model.HomePageResponse>> homePage = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.MutableLiveData<java.util.List<java.lang.String>> _availableProviders = null;
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.LiveData<java.util.List<java.lang.String>> availableProviders = null;
     
     @javax.inject.Inject()
     public HomeViewModel(@org.jetbrains.annotations.NotNull()
@@ -31,6 +39,16 @@ public final class HomeViewModel extends com.spiderybook.ui.common.BaseViewModel
     
     @org.jetbrains.annotations.NotNull()
     public final androidx.lifecycle.LiveData<java.lang.String> getSelectedProvider() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<com.spiderybook.util.Resource<com.spiderybook.domain.model.HomePageResponse>> getHomePage() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.util.List<java.lang.String>> getAvailableProviders() {
         return null;
     }
     
